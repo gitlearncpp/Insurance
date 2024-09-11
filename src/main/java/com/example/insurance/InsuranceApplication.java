29dfd526dfcbb3a645cf2a7a0a09e807c853ea37
cisco <subartowicz@gmail.com>
2024-09-11 13:46:49 +0200

    f

--- src/main/java/com/example/insurance/InsuranceApplication.java
+++ src/main/java/com/example/insurance/InsuranceApplication.java
@@ -2,7 +2,7 @@ package com.example.insurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// Uruchomienia aplikacji - tutaj .111
+// Uruchomienia aplikacji - tutaj .1111
 @SpringBootApplication
 public class InsuranceApplication {
 
